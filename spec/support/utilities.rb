88d122d9694d53ef0b1a27f9d300fdf49f2824a5
kode29 <kyle@shadowdev.com>
2013-08-11 20:11:41 -0400

    Polished up the CSS, pages, and push before the Users sign in integration

---
+++ spec/support/utilities.rb
@@ -0,0 +1,8 @@
+	def full_title(page_title)					#Documentation comment
+		base_title = "Ruby on Rails Tutorial Sample App"	#Method definition
+		if page_title.empty?					# Variable assignment
+			base_title					# Boolen test
+		else
+			"#{base_title} | #{page_title}"			# String interpoliation
+		end
+	end
